c398fd4c114140294f31c444f814bef0464293db
Erik J. Sturcke <erik@sturcke.co>
2016-11-12 23:21:41 -0500

    Enable CORS

--- rest-api/index.js
+++ rest-api/index.js
@@ -14,6 +14,24 @@ app.use(bodyParser.json())
 app.use(bodyParser.json({ type : 'application/vnd.api+json' }))
 app.use(methodOverride())
 app.use(compression())
+app.use((req, res, next) => {
+
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", url.format(appHostInfo))
+
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next()
+})
 
 const mongoose = restful.mongoose
 mongoose.Promise = global.Promise
