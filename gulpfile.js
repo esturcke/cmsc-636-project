6a2ac76d4a55f98fc9f52ac8eb8eb0a68a780528
Erik J. Sturcke <erik@sturcke.co>
2016-11-12 23:36:07 -0500

    Remove gulp

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp  = require("gulp")
-const shell = require("gulp-shell")
-
-const MONGO_DATA = "./rest-api/data/"
-
-gulp.task("mongo-start", shell.task([
-  `mkdir -p ${MONGO_DATA}`,
-  `mongod --bind_ip localhost --dbpath ${MONGO_DATA} --quiet`,
-]))
