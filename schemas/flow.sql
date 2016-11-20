d1816a082c73790f62f2c7cf96e8b39122b0ad26
Erik J. Sturcke <erik@sturcke.co>
2016-11-20 16:25:19 -0500

    Add `flow` and `host` schemas

---
+++ schemas/flow.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+DROP TABLE flow;
+DROP TYPE protocol;
+
+CREATE TYPE protocol AS ENUM ('tcp', 'udp', 'icmp');
+
+CREATE TABLE flow (
+  id              serial PRIMARY KEY,
+  protocol        protocol NOT NULL,
+  srcIp           inet     NOT NULL,
+  srcPort         integer  NOT NULL,
+  dstIp           inet     NOT NULL,
+  dstPort         integer  NOT NULL,
+  duration        integer  NOT NULL,
+  srcPayloadBytes integer  NOT NULL,
+  srcTotalBytes   integer  NOT NULL,
+  dstPayloadBytes integer  NOT NULL,
+  dstTotalBytes   integer  NOT NULL,
+  srcPacketCount  integer  NOT NULL,
+  dstPacketCount  integer  NOT NULL,
+  forcedOut       integer  NOT NULL
+);
+
+COMMIT;
---
+++ schemas/host.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+CREATE TYPE enterpriseSite AS ENUM ('1', '2', '3');
+CREATE TYPE hostType AS ENUM ('workstation', 'administrator', 'server');
+CREATE TYPE serverService AS ENUM ('http', 'smtp', 'domain');
+
+CREATE TABLE host (
+  ip       inet PRIMARY KEY,
+  name     text NOT NULL,
+  nickName text NOT NULL,
+  site     enterpriseSite NOT NULL,
+  type     hostType NOT NULL,
+  service  serverService NOT NULL
+);
+
+COMMIT;
