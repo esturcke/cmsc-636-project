840bc293d57ee4477b5fe4dbb5adf4742ef525a8
Erik J. Sturcke <erik@sturcke.co>
2016-11-20 21:04:08 -0500

    Process flows and output as CSV to STDOUT

--- bin/process-flows.js
+++ bin/process-flows.js
@@ -1,15 +1,10 @@
 import fs                     from "fs"
 import es                     from "event-stream"
 import { toLong as ipToLong } from "ip"
-import mongoose               from "mongoose"
 import multistream            from "multistream"
-import { Flow as Resource }   from "~/model"
 
-mongoose.Promise = global.Promise
-
-const MONGO = "mongodb://localhost:27017/test"
 const FILES = [
-  "./raw-vast-data/100k-flows.csv",
+  "./raw-vast-data/100-flows.csv",
   /*
   "./raw-vast-data/nf/nf-chunk1.csv",
   "./raw-vast-data/nf/nf-chunk2.csv",
@@ -35,45 +30,28 @@ const construct = es.map((line, cb) => {
   if (parseInt(flow[9]))  console.log(`More fragment: ${flow[9]}`)
   if (parseInt(flow[10])) console.log(`Cont fragment: ${flow[10]}`)
 
-  cb(null, {
-    time            : flow[0] * 1000,
-    protocol        : protocol(flow[3]),
-    srcIp           : ipToLong(flow[5]),
-    dstIp           : ipToLong(flow[6]),
-    srcPort         : parseInt(flow[7]),
-    dstPort         : parseInt(flow[8]),
-    duration        : parseInt(flow[11]),
-    srcPayloadBytes : parseInt(flow[12]),
-    srcTotalBytes   : parseInt(flow[12]),
-    dstPayloadBytes : parseInt(flow[12]),
-    dstTotalBytes   : parseInt(flow[12]),
-    srcPacketCount  : parseInt(flow[13]),
-    dstPacketCount  : parseInt(flow[14]),
-    forcedOut       : parseInt(flow[15]),
-  })
+  cb(null, [
+    flow[0] * 1000,      // time
+    protocol(flow[3]),   // protocol
+    ipToLong(flow[5]),   // srcIp
+    ipToLong(flow[6]),   // dstIp
+    parseInt(flow[7]),   // srcPort
+    parseInt(flow[8]),   // dstPort
+    parseInt(flow[11]),  // duration
+    parseInt(flow[12]),  // srcPayloadBytes
+    parseInt(flow[12]),  // srcTotalBytes
+    parseInt(flow[12]),  // dstPayloadBytes
+    parseInt(flow[12]),  // dstTotalBytes
+    parseInt(flow[13]),  // srcPacketCount
+    parseInt(flow[14]),  // dstPacketCount
+    parseInt(flow[15]),  // forcedOut
+  ].join(",") + "\n")
 })
 
 const removeJunk = pattern => es.map((line, cb) => { line.match(pattern) ? cb() : cb(null, line) })
-const stream = () => multistream(FILES.map(file => fs.createReadStream(file)))
+
+multistream(FILES.map(file => fs.createReadStream(file)))
   .pipe(es.split())
   .pipe(removeJunk(/^[\D]/))
   .pipe(construct)
-
-const insert = (objects, done) => {
-  objects.pipe(es.map((object, cb) => {
-    new Resource(object).save(err => {
-      if (err) console.error(err)
-      cb()
-    })
-  }))
-  .pipe(es.wait(() => done()))
-}
-
-mongoose.connect(MONGO)
-const db = mongoose.connection
-db.on("error", console.error.bind(console, "connection error:"))
-db.once("open", () => {
-  Resource.remove({}, () => {
-    insert(stream(), () => { mongoose.disconnect() })
-  })
-})
+  .pipe(process.stdout)
