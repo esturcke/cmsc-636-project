d6d1f49485f154636600a2c9ddf7f51bd5e87f13
Erik J. Sturcke <erik@sturcke.co>
2016-11-22 00:16:04 -0500

    Add time to flow schema

--- schemas/flow.sql
+++ schemas/flow.sql
@@ -7,19 +7,22 @@ CREATE TYPE protocol AS ENUM ('tcp', 'udp', 'icmp');
 
 CREATE TABLE flow (
   id              serial PRIMARY KEY,
-  protocol        protocol NOT NULL,
-  srcIp           inet     NOT NULL,
-  srcPort         integer  NOT NULL,
-  dstIp           inet     NOT NULL,
-  dstPort         integer  NOT NULL,
-  duration        integer  NOT NULL,
-  srcPayloadBytes integer  NOT NULL,
-  srcTotalBytes   integer  NOT NULL,
-  dstPayloadBytes integer  NOT NULL,
-  dstTotalBytes   integer  NOT NULL,
-  srcPacketCount  integer  NOT NULL,
-  dstPacketCount  integer  NOT NULL,
-  forcedOut       integer  NOT NULL
+  time            integer   NOT NULL,
+  protocol        protocol  NOT NULL,
+  srcIp           inet      NOT NULL,
+  srcPort         integer   NOT NULL,
+  dstIp           inet      NOT NULL,
+  dstPort         integer   NOT NULL,
+  moreFragment    integer   NOT NULL,
+  contFragment    integer   NOT NULL,
+  duration        integer   NOT NULL,
+  srcPayloadBytes integer   NOT NULL,
+  srcTotalBytes   integer   NOT NULL,
+  dstPayloadBytes integer   NOT NULL,
+  dstTotalBytes   integer   NOT NULL,
+  srcPacketCount  integer   NOT NULL,
+  dstPacketCount  integer   NOT NULL,
+  forcedOut       integer   NOT NULL
 );
 
 COMMIT;
