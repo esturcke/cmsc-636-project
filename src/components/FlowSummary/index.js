d3936f2b7fd67e0b359b952efc0d583bafa78e6a
Erik J. Sturcke <erik@sturcke.co>
2016-12-07 02:35:30 -0500

    Format time selector to look like a calendar

--- src/components/FlowSummary/index.js
+++ src/components/FlowSummary/index.js
@@ -1,13 +1,14 @@
 import React                     from "react"
 import { scaleLog, scaleLinear } from "d3-scale"
+import { range }                 from "lodash"
 import Path                      from "~/components/svg/Path"
 import T                         from "~/lib/types"
 import styles                    from "./flow-summary.scss"
 
 const width  = 1000
-const height = 100
-const padding = { top : 5, left : 15, right : 15 }
-const trafficScale = scaleLog().domain([1, 1000]).range([0.1, height / 2]).clamp(true)
+const height = 60
+const padding = { top : 30, left : 10, right : 10 }
+const trafficScale = scaleLog().domain([1, 700]).range([0.1, height / 2]).clamp(true)
 const denyScale    = scaleLog().domain([5000, 50000]).range([0.1, 15]).clamp(true)
 
 const FlowSummary = ({ flows, intrusions, from, to, updateSpan }) => {
@@ -18,9 +19,34 @@ const FlowSummary = ({ flows, intrusions, from, to, updateSpan }) => {
     x     : 0,
     width : timeScale(flows[1].time) - timeScale(flows[0].time),
   }
+  const format = new Intl.DateTimeFormat(undefined, {
+    weekday : "short",
+  }).format
 
   return (
     <g transform={`translate(${padding.left},${padding.top})`}>
+      {range(
+        new Date(timeSpan[0] * 1000).setHours(0,0,0,0) / 1000 - 5 * 3600,
+        timeSpan[1] - 5 * 3600,
+        24 * 3600,
+      ).map((time, i) => {
+        const start = timeScale(time)
+        const eight = timeScale(time + 8 * 3600)
+        const five  = timeScale(time + 17 * 3600)
+        const end   = timeScale(time + 24 * 3600)
+        const day   = format(new Date(time * 1000))
+        return (
+					<g key={i}>
+						<rect x={start} width={eight - start} y={0} height={height} className={styles.offHours}/>
+            <rect x={five} width={end - five} y={0} height={height} className={styles.offHours}/>
+            <Path d={`M ${start} 0 l 0 ${height}`} className={styles.dateMarker}/>
+            <Path d={`M ${eight} 0 l 0 ${height}`} className={styles.workdayMarker} strokeDasharray="1,5"/>
+            <Path d={`M ${five} 0 l 0 ${height}`} className={styles.workdayMarker} strokeDasharray="1,5"/>
+            <rect x={start} width={end - start} y={-15} height={15} className={styles.date}/>
+            <text x={(start + end) / 2} y={-2} fill="#333" textAnchor="middle">{day}</text>
+          </g>
+				)
+      })}
       {from && to ? <Path d={`M ${timeScale(from)} 0 l 0 ${height} L ${timeScale(to)} ${height} l 0 ${-height} Z`} className={styles.current}/> : null}
       {flows.map(({ time, mbps_inbound, mbps_outbound }, i) => (
         <g key={i} transform={`translate(${timeScale(time)},${height / 2})`}>
