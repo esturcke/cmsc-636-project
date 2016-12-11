d5020281428e335ff9b633aeb4d8c8d3a2a34a31
Erik J. Sturcke <erik@sturcke.co>
2016-12-11 14:23:10 -0500

    Format traffic and connection columns in tables

--- src/components/formatters/Traffic/index.js
+++ src/components/formatters/Traffic/index.js
@@ -1,13 +1,21 @@
-import React  from "react"
-import T      from "~/lib/types"
-import styles from "./traffic.scss"
+import React      from "react"
+import filesize   from "filesize"
+import { repeat } from "lodash"
+import T          from "~/lib/types"
+import styles     from "./traffic.scss"
 
-const Traffic = ({ mbps }) => mbps ? (
-  <span className={styles.wrapper}>
-    <span className={styles.value}>{(mbps * 1000).toLocaleString(undefined, { maximumSignificantDigits : 2 })}</span>
-    <span className={styles.units}>kbps</span>
-  </span>
-) : null
+const align = value => repeat("\u2007", 5 - value.replace(/\..*$/, "").length) + value
+
+const Traffic = ({ mbps }) => {
+  if (!mbps) return null
+  const { value, suffix } = filesize(mbps * 1000000, { bits : true, base : 10, output : "object" })
+  return (
+    <span className={styles.wrapper}>
+      <span className={styles.value}>{align(value.toLocaleString(undefined, { maximumSignificantDigits : 2 }))}</span>
+      <span className={styles.units}>{suffix + "ps"}</span>
+    </span>
+  )
+}
 
 Traffic.propTypes = {
   mbps : T.number,
