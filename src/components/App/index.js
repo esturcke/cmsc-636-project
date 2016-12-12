df40ba0cd0c6ee61e1e57db7ba0a0f4226ddee70
Erik J. Sturcke <erik@sturcke.co>
2016-12-12 03:19:36 -0500

    Remove duplicate prop

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -89,7 +89,7 @@ class App extends React.Component {
         />
         <div className={styles.tables}>
           <HostTable hosts={hosts} hostStats={hostStats} setShowOnly={this.setShowOnly} showOnly={showOnly}/>
-          <FlowTable flows={flows} showOnly={showOnly} setShowOnly={this.setShowOnly} showOnly={showOnly}/>
+          <FlowTable flows={flows} showOnly={showOnly} setShowOnly={this.setShowOnly}/>
         </div>
         <Loading loading={loading}/>
       </div>
