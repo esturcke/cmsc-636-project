5b0400a6945aa5800c126c5a9ca477faa6f655bb
Erik J. Sturcke <erik@sturcke.co>
2016-12-12 02:10:51 -0500

    Add host table sorting

--- src/components/HostTable/index.js
+++ src/components/HostTable/index.js
@@ -1,43 +1,57 @@
-import React                        from "react"
-import { AutoSizer, Column, Table } from "react-virtualized"
-import { get }                      from "lodash"
-import { naturalSort }              from "javascript-natural-sort"
-import Traffic                      from "~/components/formatters/Traffic"
-import T                            from "~/lib/types"
-import styles                       from "./host-table.scss"
+import React                                       from "react"
+import { AutoSizer, Column, Table, SortDirection } from "react-virtualized"
+import { get, values, map }                        from "lodash"
+import naturalSort                                 from "javascript-natural-sort"
+import Traffic                                     from "~/components/formatters/Traffic"
+import T                                           from "~/lib/types"
+import styles                                      from "./host-table.scss"
 
-const HostTable = ({ hosts = {}, hostStats = {} }) => {
-  const ips = Object.keys(hosts).sort(naturalSort)
-  return (
-    <div className={styles.table}><AutoSizer>{({ height, width }) => (
-      <Table
-        width={width}
-        height={height}
-        headerHeight={20}
-        rowHeight={20}
-        rowCount={ips.length}
-        rowGetter={({ index }) => ({ ...hostStats[ips[index]], ...hosts[ips[index]] })}
-      >
-        <Column
-          label="IP"
-          dataKey="ip"
-          width={100}
-        />
-        <Column
-          label="Traffic In"
-          dataKey="traffic-in"
-          cellRenderer={({ rowData }) => <Traffic mbps={get(rowData, "traffic.in", 0)}/>}
-          width={100}
-        />
-        <Column
-          label="Traffic Out"
-          dataKey="traffic-out"
-          cellRenderer={({ rowData }) => <Traffic mbps={get(rowData, "traffic.out", 0)}/>}
-          width={100}
-        />
-      </Table>
-    )}</AutoSizer></div>
-  )
+const order = (dataKey, direction) => (a, b) => (direction === SortDirection.ASC ? 1 : -1) * naturalSort(get(a, dataKey), get(b, dataKey))
+
+class HostTable extends React.Component {
+  state = { sortBy : "ip", sortDirection : SortDirection.ASC }
+
+  sort = newSort => this.setState(newSort)
+
+  render() {
+    const { hosts = {}, hostStats = {} } = this.props
+    const { sortBy, sortDirection } = this.state
+    const data = map(values(hosts), host => ({ ...hostStats[host.ip], ...host })).sort(order(sortBy, sortDirection))
+
+    return (
+      <div className={styles.table}><AutoSizer>{({ height, width }) => (
+        <Table
+          width={width}
+          height={height}
+          headerHeight={20}
+          rowHeight={20}
+          rowCount={data.length}
+          rowGetter={({ index }) => data[index]}
+          sortBy={sortBy}
+          sortDirection={sortDirection}
+          sort={this.sort}
+        >
+          <Column
+            label="IP"
+            dataKey="ip"
+            width={100}
+          />
+          <Column
+            label="Traffic In"
+            dataKey="traffic.in"
+            cellRenderer={({ rowData }) => <Traffic mbps={get(rowData, "traffic.in", 0)}/>}
+            width={100}
+          />
+          <Column
+            label="Traffic Out"
+            dataKey="traffic.out"
+            cellRenderer={({ rowData }) => <Traffic mbps={get(rowData, "traffic.out", 0)}/>}
+            width={100}
+          />
+        </Table>
+      )}</AutoSizer></div>
+    )
+  }
 }
 
 HostTable.propTypes = {
